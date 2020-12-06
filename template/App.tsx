2baed1f1898c43a35ad8ab4b401010838dd35453
littlepotato123 <sopanda0910@gmail.com>
2020-12-06 08:04:54 -0500

    Added Template App

---
+++ template/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Template for Mars Rover</Text>
+    </View>
+  );
+};
