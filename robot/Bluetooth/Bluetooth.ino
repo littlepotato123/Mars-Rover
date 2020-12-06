bb75286f83f07fa9e1ec2fb6db1262999e0026df
littlepotato123 <sopanda0910@gmail.com>
2020-12-05 20:50:33 -0500

    Added Code Samples

---
+++ robot/Bluetooth/Bluetooth.ino
@@ -0,0 +1,15 @@
+int LED = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()) {
+    char c = Serial.read();
+    if(c=='1') {
+      digitalWrite(LED, HIGH);
+    }
+  }
+}
