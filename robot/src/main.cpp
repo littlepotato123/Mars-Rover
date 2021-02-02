af4f8696909a97a0af5927da122e7394adb2bfa8
littlepotato123 <sopanda0910@gmail.com>
2021-02-02 17:32:56 -0500

    Added Keybinds in Code

--- robot/src/main.cpp
+++ robot/src/main.cpp
@@ -10,7 +10,14 @@ void setup() {
 void loop() {
   if(Serial.available()) {
     int value = Serial.read();
-    if(value == '1') digitalWrite(LED, HIGH);
-    else if (value == '0') digitalWrite(LED, LOW);
+    if(value == '0') digitalWrite(LED, LOW);
+    else if (value == '1') digitalWrite(LED, HIGH);
+    else if (value == '2'); // Servo at 0 Degrees
+    else if (value == '3'); // Servo at 90 Degrees
+    else if (value == '4'); // Stepper Motor at 0 Degrees
+    else if (value == '5'); // Stepper Motor at 90 Degrees
+    else if (value == '6'); // Stepper Motor at 180 Degrees
+    else if (value == '7'); // Stepper Motor at 270 Degrees
+    else if (value == '8'); // Stepper Motor at 360 Degrees
   }
 }
